8929c12b068fded0332bdbf10c87b87ec6c9b785
Holdest White <zbsholdest@mail.ru>
2023-06-13 19:58:02 +0300

    added new drag

--- groups/drag.js
+++ groups/drag.js
@@ -1,3 +1,104 @@
+let row;
+
+class Drag{
+
+    dragRow;
+
+    column = [];
+
+    constructor() {
+
+    }
+
+    append = (elem) => {
+        this.column.push(elem)
+
+        elem.setAttribute('draggable', true)
+        elem.addEventListener('dragstart', (ev) => this.dragStart(ev))
+        elem.addEventListener('dragover', (ev) => this.dragOver(ev))
+        elem.addEventListener('dragend', (ev) => this.dragEnd(ev))
+        // elem.setAttribute('ondragstart', )
+        // elem.setAttribute('ondragover', (event) => this.dragOver(event))
+    }
+
+    dragStart = (event) => {
+
+
+        this.dragRow = event.target
+
+        // let crt = event.target.cloneNode(true)
+        // crt.style.opacity = 10;
+
+        // document.body.appendChild(crt);
+        // event.dataTransfer.setDragImage(crt, 0, 0)
+
+        setTimeout(() => {
+            this.dragRow.classList.add('hide')
+        });
+
+
+
+
+
+        console.log(this.dragRow)
+
+        return false
+    }
+
+    dragEnd = (event) => {
+
+        this.dragRow.classList.remove('hide')
+    }
+
+    dragOver = (event) => {
+        event.preventDefault();
+
+        let target = event.target;
+
+        while (!target.classList.contains("group-team-card")){
+            target = target.parentNode
+        }
+
+        if(target.id === this.dragRow.id){
+            return
+        }
+
+        this.swapElems(this.dragRow, target)
+    }
+
+    swapElems = (first, second) => {
+
+        let firstIndex = this.column.findIndex((elem) => elem.id === first.id)
+        let secondIndex = this.column.findIndex((elem) => elem.id === second.id)
+
+        if(firstIndex !== -1 && secondIndex !== -1){
+            this.column[firstIndex] = second;
+            this.column[secondIndex] = first;
+
+            this.swapElemsInHtml(first, second)
+        }
+
+        console.log(this.column)
+    }
+
+    swapElemsInHtml = (first, second) => {
+        let firstRowParent = first.parentNode
+
+        second.parentNode.append(first);
+        firstRowParent.append(second)
+    }
+}
+
+let dragB = new Drag();
+
+dragB.append(document.getElementById("member6"))
+dragB.append(document.getElementById("member7"))
+dragB.append(document.getElementById("member8"))
+dragB.append(document.getElementById("member9"))
+dragB.append(document.getElementById("member10"))
+
+
+
 function dragStart(event)
 {
     row = event.target;
@@ -73,8 +174,8 @@ const setPredictionResult = (team, result) => {
 
 // не забудь, что нужны id в верстке. id от драга подойдут
 //пример
-const temp = 6;
-setPredictionResult(document.querySelector(`#member${temp}`), 1)
+// const temp = 6;
+// setPredictionResult(document.querySelector(`#member${temp}`), 1)
 //пример
-setPredictionResult(document.querySelector(`#member${1}`), 0)
+// setPredictionResult(document.querySelector(`#member${1}`), 0)
 
